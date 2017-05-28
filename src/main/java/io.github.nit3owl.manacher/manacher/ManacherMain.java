5904de61ae837b487af07598425c4be2363719bd
nit3owl <nit3owldev@gmail.com>
2017-05-28 12:46:32 -0700

    Minor formatting and style updates

--- src/main/java/io.github.nit3owl.manacher/manacher/ManacherMain.java
+++ src/main/java/io.github.nit3owl.manacher/manacher/ManacherMain.java
@@ -1,18 +1,18 @@
 package io.github.nit3owl.manacher.manacher;
 
-public class ManacherMain{
+public class ManacherMain {
 
-	private static final String USAGE_MESSAGE = "usage: java ManacherMain <string>";
-	
-    public static void main(String[] args){
-    	String test = null;
-    	if(args.length > 0)
-    		test = args[0];
-    	else{
-    		System.out.println(USAGE_MESSAGE);
-    		System.exit(-1);
-    	}    		
-        System.out.printf("The first, longest palindrome in %s is %s.\n", test, 
-                            Manacher.findLongestPalindrome(test));
+    private static final String USAGE_MESSAGE = "usage: java ManacherMain <string>";
+
+    public static void main(String[] args) {
+        String test = null;
+        if (args.length > 0)
+            test = args[0];
+        else {
+            System.out.println(USAGE_MESSAGE);
+            System.exit(-1);
+        }
+        System.out.printf("The first, longest palindrome in %s is %s.\n", test,
+                Manacher.findLongestPalindrome(test));
     }
 }
